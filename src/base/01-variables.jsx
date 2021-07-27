0c50aaba7e263fab0abd4c280effc4043ce6557a
David Merchan <juan1014166@gmail.com>
2021-07-26 19:21:54 -0500

    variables in js

---
+++ src/base/01-variables.jsx
@@ -0,0 +1,13 @@
+// Variables y Constantes
+const name = 'Nikola';
+const apellido = 'Tesla';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(name, apellido, valorDado);
+
+if (true) {
+  let valorDado = 6;
+  console.log(valorDado);
+}
